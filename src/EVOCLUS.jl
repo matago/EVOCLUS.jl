e51cdf3b4b11632d6359c8f9472e4aa5204fe970
Matthew Agostinelli <matthew.agostinelli@gmail.com>
2017-03-23 14:32:52 -0400

    Export mutation and recombinations

--- src/EVOCLUS.jl
+++ src/EVOCLUS.jl
@@ -1,5 +1,15 @@
 module EVOCLUS
 
-# package code goes here
+  export # Mutations
+         inversion!, insertion!, swap2!, scramble!, shifting!,
+         # Recombinations
+         pmx, cx, ox1, ox2, pos
+
+
+  ## Recombinations
+  include("recombinations.jl")
+  ## Mutations
+  include("mutations.jl")
 
 end # module
+
