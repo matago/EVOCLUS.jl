15f72fe74bbadb246d1e581e1b654d1d7f047508
Matthew Agostinelli <matthew.agostinelli@gmail.com>
2017-03-24 14:13:43 -0400

    Selection exports

--- src/EVOCLUS.jl
+++ src/EVOCLUS.jl
@@ -5,7 +5,9 @@ module EVOCLUS
   export # Mutations
          inversion!, insertion!, swap2!, scramble!, shifting!,
          # Recombinations
-         pmx, cx, ox1, ox2, pos
+         pmx, cx, ox1, ox2, pos,
+         # Selections
+         ranklinear, tournament, roulette, sus
 
 
   ## Recombinations
