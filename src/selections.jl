85d8662561114a4522369572d12c2c1cd1bb7f33
Matthew Agostinelli <matthew.agostinelli@gmail.com>
2017-03-25 13:09:00 -0400

    All selections based on max

--- src/selections.jl
+++ src/selections.jl
@@ -6,7 +6,7 @@ function ranklinear(sp::Float64)
   @assert 1.0 <= sp <= 2.0 "Selective pressure has to be in range [1.0, 2.0]."
   function rank(fitness::Vector{Float64}, N::Int)
       λ = float(length(fitness))
-      idx = sortperm(fitness,rev=true)
+      idx = sortperm(fitness)
       ranks = zeros(Integer(λ))
       for i in 1:Integer(λ)
           ranks[i] = ( 2.0- sp + 2.0*(sp - 1.0)*(idx[i] - 1.0) / (λ - 1.0) ) /λ
@@ -29,7 +29,7 @@ function tournament(groupSize::Integer)
       wf = fitness[winner]
       for k in 2:groupSize
         contender = contenders[k]
-        if fitness[contender] < wf
+        if fitness[contender] > wf
           winner = contender
           wf = fitness[contender]
         end
@@ -44,7 +44,7 @@ end
 # Roulette wheel (proportionate selection) selection
 function roulette(fitness::Vector{Float64}, N::Integer)
   n = length(fitness)
-  wv = weights(fitness.^-1)
+  wv = weights(fitness)
   sample(1:n,wv,N,replace = true)
 end
 
@@ -52,7 +52,7 @@ end
 function sus(fitness::Vector{Float64}, N::Integer)
   n = length(fitness)
   @assert N <= n "Number of selections (N) must be less than numeber of elements"
-  wv = weights(fitness.^-1)
+  wv = weights(fitness)
   sample(1:n,wv,N,replace = false)
 end
 
