62ee367f3c7767599daf61a42389cece3b2df2e6
Matthew Agostinelli <matthew.agostinelli@gmail.com>
2017-04-30 00:20:31 -0400

    Corrected final edge addition

--- src/greedy.jl
+++ src/greedy.jl
@@ -6,21 +6,34 @@ function greedy{T<:Real}(distmat::Matrix{T})
   ordDist = sortperm(vec(distmat))
   G = Graph(numCities)
   k = 1
-  while δ(G) < 2
+  #main loop
+  while ne(G) < numCities-1
     dnode,anode = ind2sub(dmsz,ordDist[k])
     if dnode != anode
       d°,a° = degree(G,dnode),degree(G,anode)
       if d° < 2 && a° < 2
-        if d° == 0 || a° == 0
+        if d° == 0 && a° == 0
           add_edge!(G,dnode,anode)
-        elseif !in(dnode,saw(G,anode,ne(G)))
+        elseif independent(G,dnode,anode)
           add_edge!(G,dnode,anode)
         end
       end
     end
-  k+=1
+    k+=1
   end
-  return saw(G,1,numCities)
+  #final edge addition
+  add_edge!(G,findin(degree(G),1)...)
+  return G
+  # return saw(G,1,numCities)
 end
 
+function independent(G::Graph,dnode::Int,anode::Int)
+  valid = true
+  for vset in connected_components(G)
+    if valid && in(dnode,vset) && in(anode,vset)
+      valid = false
+    end
+  end
+  return valid
+end
 
